4c5a7838e3c925e8a2edab029966870b3c07ba29
gabrielmmh <gabrielmmh@al.insper.edu.br>
2023-06-04 14:14:27 -0300

    next

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\Gabriel\\Desktop\\INSPER\\4ºsemestre\\TECNOLOGIAS_WEB\\Projetos\\Projeto3\\tocket-frontend\\app\\layout.jsx": [
+      "C:\\Users\\Gabriel\\Desktop\\INSPER\\4ºsemestre\\TECNOLOGIAS_WEB\\Projetos\\Projeto3\\tocket-frontend\\styles\\globals.css"
+    ],
+    "C:\\Users\\Gabriel\\Desktop\\INSPER\\4ºsemestre\\TECNOLOGIAS_WEB\\Projetos\\Projeto3\\tocket-frontend\\app\\events\\[id]\\page.jsx": [
+      "C:\\Users\\Gabriel\\Desktop\\INSPER\\4ºsemestre\\TECNOLOGIAS_WEB\\Projetos\\Projeto3\\tocket-frontend\\styles\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\Gabriel\\Desktop\\INSPER\\4ºsemestre\\TECNOLOGIAS_WEB\\Projetos\\Projeto3\\tocket-frontend\\app\\page": [
+      "C:\\Users\\Gabriel\\Desktop\\INSPER\\4ºsemestre\\TECNOLOGIAS_WEB\\Projetos\\Projeto3\\tocket-frontend\\styles\\globals.css"
+    ],
+    "C:\\Users\\Gabriel\\Desktop\\INSPER\\4ºsemestre\\TECNOLOGIAS_WEB\\Projetos\\Projeto3\\tocket-frontend\\app\\events\\[id]\\page": [
+      "C:\\Users\\Gabriel\\Desktop\\INSPER\\4ºsemestre\\TECNOLOGIAS_WEB\\Projetos\\Projeto3\\tocket-frontend\\styles\\globals.css"
+    ]
+  }
+}
